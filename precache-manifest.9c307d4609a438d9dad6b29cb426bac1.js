05f0eabab0250113e70d312c0a8c2e3b46890c8d
Tuan Pham Anh <baabeetaa@gmail.com>
2018-10-17 16:11:07 +0700

    Updates

---
+++ precache-manifest.9c307d4609a438d9dad6b29cb426bac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a604b95d60d2223f97c",
+    "url": "/static/js/main.0a604b95.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "0a604b95d60d2223f97c",
+    "url": "/static/css/main.02504501.chunk.css"
+  },
+  {
+    "revision": "1aafaec4997e96ce1988a6b74ec8c64d",
+    "url": "/index.html"
+  }
+];
